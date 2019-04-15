d68dced01542744a06b1a9bc253c45d214dc709d
Aslan Varoqua <aslan.varoqua@gmail.om>
2019-04-15 11:05:22 -0600

    refID

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import request, url_for
 from flask_api import FlaskAPI, status, exceptions
+import pymongo
+import uuid
 
 app = FlaskAPI(__name__)
 
@@ -9,11 +11,18 @@ def home():
     Gets latitude and longitude from POST request
     """
     req_data = request.get_json()
-    latitude = req_data['latitude']
-    longitude = req_data['longitude']
-    print(latitude)
-    print(longitude)
-    return "Latitude: " + str(latitude) + " , Longitude: " + str(longitude)
+    lat1 = req_data['lat1']
+    lon1 = req_data['lon1']
+
+
+
+    print(lat1)
+    print(lon1)
+    refId = uuid.uuid4()
+
+    # start calculations
+
+    return "Latitude: " + str(lat1) + " , Longitude: " + str(lon1) + "id = " + refId
 
 if __name__ == "__main__":
     app.run(debug=True)
